a8c49778dddffd6d3b978252f6d1aa107890710a
slehmadi <81602663+slehmadi@users.noreply.github.com>
2021-04-19 13:01:31 +0800

    Update save.py

--- save.py
+++ save.py
@@ -1,4 +1,4 @@
-import os, utility, variabelGlobal
+import os, time, utility, variabelGlobal
 def Save():
     #I.S. variabel user,gadget,gadget_return_history, gadget_borrow_history, consumable, consumable_history sudah terdefinisi
     #F.S. variabel-variabel di atas akan disimpan di file csv yang berkaitan
@@ -10,6 +10,9 @@ def Save():
         utility.SimpanCSV(variabelGlobal.gadget_return_history['header'], variabelGlobal.gadget_return_history['data'], folderDir +'/gadget_return_history.csv')
         utility.SimpanCSV(variabelGlobal.consumable['header'], variabelGlobal.consumable['data'], folderDir +'/consumable.csv')
         utility.SimpanCSV(variabelGlobal.consumable_history['header'], variabelGlobal.consumable_history['data'], folderDir +'/consumable_history.csv')
+        print("saving..")
+        time.sleep(2)
+        print("Data telah disimpan pada folder {}".format(folderDir))
     else:
         os.makedirs(folderDir)
         utility.SimpanCSV(variabelGlobal.user['header'], variabelGlobal.user['data'], folderDir +'/user.csv')
@@ -18,3 +21,6 @@ def Save():
         utility.SimpanCSV(variabelGlobal.gadget_return_history['header'], variabelGlobal.gadget_return_history['data'], folderDir +'/gadget_return_history.csv')
         utility.SimpanCSV(variabelGlobal.consumable['header'], variabelGlobal.consumable['data'], folderDir +'/consumable.csv')
         utility.SimpanCSV(variabelGlobal.consumable_history['header'], variabelGlobal.consumable_history['data'], folderDir +'/consumable_history.csv')
+        print("saving..")
+        time.sleep(2)
+        print("Data telah disimpan pada folder {}".format(folderDir))
